7f8120bc275745b3a304963cd81c7dbcb461ab09
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-07-12 19:47:01 +0300

    fixed issue #2 - carmine created depedency conflict with riemann

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject riemann-flapjack-output "0.0.1-SNAPSHOT"
   :description "A Riemann plugin to output events to Flapjack"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.taoensso/carmine "2.9.2"]
-                 [cheshire "5.4.0"]
+  :dependencies [
+                 [com.taoensso/carmine "2.7.0"]
                 ]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]
-                                  [riemann "0.2.9"]]}})
+  :profiles {
+  	:dev {:dependencies [
+  			[midje "1.6.3"]
+        	[riemann "0.2.9"]]}})
