e39fa1143b74ddd33a263dc55c57b4dbf03a4583
Avishai Ish-Shalom <avishai@fewbytes.com>
2015-07-12 19:52:03 +0300

    fixed dev depedencies

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject riemann-flapjack-output "0.0.1-SNAPSHOT"
   :description "A Riemann plugin to output events to Flapjack"
   :dependencies [
-                 [com.taoensso/carmine "2.7.0"]
+                 [com.taoensso/carmine "2.7.0" :exclusions [org.clojure/clojure]]
                 ]
   :profiles {
   	:dev {:dependencies [
-  			[midje "1.6.3"]
+  			[org.clojure/clojure "1.6.0"]
+  			[midje "1.6.3" :exclusions [org.clojure/clojure]]
         	[riemann "0.2.9"]]}})
