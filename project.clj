55035fdf1f05256f9938d10504b3c5afbe83d8cc
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2022-02-24 11:28:40 +0200

    Update dependencies, works with java 17

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject riemann-flapjack-output "0.0.1-SNAPSHOT"
   :description "A Riemann plugin to output events to Flapjack"
   :dependencies [
-                 [com.taoensso/carmine "2.7.0" :exclusions [org.clojure/clojure]]
+                 [com.taoensso/carmine "2.15.1" :exclusions [org.clojure/clojure]]
                 ]
   :profiles {
-  	:dev {:dependencies [[org.clojure/clojure "1.6.0"]
-                        [riemann "0.2.10"]]}}
+  	:dev {:dependencies [[org.clojure/clojure "1.10.3"]
+                        [riemann "0.3.8"]]}}
   ; For some reason Leiningen doesn't like fusesource redirect to https 
   :mirrors {"fusesource" "https://repo.fusesource.com/nexus/content/groups/public/"})
