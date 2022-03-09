4b381e5d0c4563fabcf5cae5e607228426120d37
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2022-03-09 18:48:36 +0200

    Move dependencies to provided scope

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://github.com/nukemberg/riemann-redis-output"
   :license "Apache v2"
   :dependencies [[com.taoensso/carmine "2.15.1" :exclusions [org.clojure/clojure]]]
-  :profiles {
-  	:dev {:dependencies [[org.clojure/clojure "1.10.3"]
-                        [riemann "0.3.8"]]}}
+  :profiles {:provided {:dependencies [[cheshire "5.10.2"]
+                                       [org.clojure/clojure "1.10.3"]
+                                       [riemann "0.3.8"]]}}
   ; For some reason Leiningen doesn't like fusesource redirect to https 
   :mirrors {"fusesource" "https://repo.fusesource.com/nexus/content/groups/public/"}
   :deploy-repositories {"releases" :clojars
