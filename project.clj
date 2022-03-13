5fb1fab29375a1defb248b572adc5093c06b8285
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2022-03-13 17:07:55 +0200

    use kaocha test runner

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
   :dependencies [[com.taoensso/carmine "2.15.1" :exclusions [org.clojure/clojure]]]
   :profiles {:provided {:dependencies [[cheshire "5.10.2"]
                                        [org.clojure/clojure "1.10.3"]
-                                       [riemann "0.3.8"]]}}
+                                       [riemann "0.3.8"]]}
+             :kaocha {:dependencies [[lambdaisland/kaocha "1.63.998"]]}}
+  :aliases {"kaocha" ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]}
   ; For some reason Leiningen doesn't like fusesource redirect to https 
   :mirrors {"fusesource" "https://repo.fusesource.com/nexus/content/groups/public/"}
   :deploy-repositories {"releases" :clojars
