ebc2a62dee4132090d6f8305628bafcb6bb387b6
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2022-07-26 19:12:13 +0300

    Version 0.0.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.github.nukemberg/riemann-redis-output "0.0.7"
+(defproject io.github.nukemberg/riemann-redis-output "0.0.8-SNAPSHOT"
   :description "A Riemann plugin to output events to Redis"
   :url "https://github.com/nukemberg/riemann-redis-output"
   :license "Apache v2"
